53ddba4794dc52e0e2b69737b637b86739dbcf5c
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-10-16 23:07:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <h2>We are just started our better career....</h2>
+    </div>
+  );
+};
+
+export default App;
+
+// Here we have created a component with name App
+// And we are giving him a permission to use anyhwere inside the project in line 9
+
+// named export and default export
+// each component in react should have one single parent
